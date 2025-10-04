0f5f918246da24f474d53804d5d39006fe3552ee
Rayo5555 <maroperrone@gmail.com>
2025-10-04 10:56:32 -0300

    fix

--- src/database.py
+++
@@ -1,7 +0,0 @@
-import databases
-from sqlalchemy import MetaData
-
-DATABASE_URL = "sqlite:///./test.db"
-
-database = databases.Database(DATABASE_URL)
-metadata = MetaData()
--- src/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, func
-from .database import metadata
-
-users = Table(
-    "users",
-    metadata,
-    Column("users", Integer, primary_key=True),
-)
